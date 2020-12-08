9e31b2c58cf30253ac8f05f040c28572f5a315eb
Tijs Teulings <tijs.t@hotmail.com>
2020-12-08 10:48:19 +0100

    tested cron

--- bike_sharing.py
+++ bike_sharing.py
@@ -1,14 +1,17 @@
+#!/usr/bin/python3
 import requests 
 import datetime
 import json
+import os
 
 URL = "https://www.velo-antwerpen.be/availability_map/getJsonObject"
+path = os.getcwd()
 
 r = requests.post(URL)
 data = r.json()
 datetime = datetime.datetime.now()
 
-with open("bike_sharing_data.json", "r") as fp:
+with open(path + "/bike_sharing_data.json", "r") as fp:
     try:
         json_data = json.load(fp)
     except:
@@ -16,5 +19,6 @@ with open("bike_sharing_data.json", "r") as fp:
 
 json_data[str(datetime)] = data
 
-with open("bike_sharing_data.json", "w") as fp:
+
+with open(path + "/bike_sharing_data.json", "w") as fp:
     json.dump(json_data, fp)
